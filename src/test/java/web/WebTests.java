9844360655a092c4e22676d97ef3f849e0626e44
Ziv Gadri <ziv.automation@gmail.com>
2024-07-17 13:15:20 +0300

    Creating TestFlows class and test case in WebTests

--- src/test/java/web/WebTests.java
+++ src/test/java/web/WebTests.java
@@ -1,11 +1,8 @@
 package web;
 
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
 import org.testng.annotations.Test;
 
-public class WebTests extends BaseTests {
-    private static final Logger logger = LogManager.getLogger(WebTests.class);
+public class WebTests extends TestFlows {
 
     public WebTests() {}
 
@@ -16,7 +13,6 @@ public class WebTests extends BaseTests {
 
     @Test()
     public void testWeb() {
-        logger.info("");
-
+        orkalaAsaData();
     }
 }
