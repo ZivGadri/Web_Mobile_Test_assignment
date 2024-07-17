5cc66dd3fdff410354103a684e078738ac7da580
Ziv Gadri <ziv.automation@gmail.com>
2024-07-17 21:43:10 +0300

    UiManager setting up screen size parameter and adding clickOnAcceptAllCookies and navigateToTradingPage

--- src/main/java/web/manager/UiManager.java
+++ src/main/java/web/manager/UiManager.java
@@ -2,19 +2,33 @@ package web.manager;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import org.openqa.selenium.Dimension;
 import org.openqa.selenium.WebDriver;
 import web.manager.pageHelpers.MainPageHelper;
 
 public class UiManager {
     private static final Logger logger = LogManager.getLogger(UiManager.class);
+    private Dimension screenSize;
     private final WebDriver driver;
+    private MainPageHelper mainPageHelper;
     private final String xmUrl = "https://www.xm.com";
 
     public UiManager(WebDriver driver) {
         this.driver = driver;
+        this.screenSize = driver.manage().window().getSize();
     }
 
     public void navigateToMainPage() {
-        MainPageHelper mainPageHelper = new MainPageHelper(driver, xmUrl);
+        mainPageHelper = new MainPageHelper(driver, xmUrl, screenSize);
     }
+
+    public void clickOnAcceptAllCookies() {
+        mainPageHelper.clickAcceptAllCookiesIfFoundOnPage();
+    }
+
+    public void navigateToTradingPage() {
+
+    }
+
+
 }
