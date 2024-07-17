2ad74a93165ad272f2deeb72fd11cf436e502b49
Ziv Gadri <ziv.automation@gmail.com>
2024-07-17 22:40:33 +0300

    AndroidDriverManager setup

--- src/main/java/mobile/manager/drivers/AndroidDriverManager.java
+++ src/main/java/mobile/manager/drivers/AndroidDriverManager.java
@@ -2,7 +2,6 @@ package mobile.manager.drivers;
 
 import io.appium.java_client.android.AndroidDriver;
 import io.appium.java_client.android.options.UiAutomator2Options;
-import io.appium.java_client.gecko.options.GeckoOptions;
 import io.appium.java_client.remote.AutomationName;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
@@ -11,18 +10,16 @@ import java.net.MalformedURLException;
 import java.net.URL;
 import java.nio.file.Path;
 import java.time.Duration;
-import java.util.HashMap;
 
 import static mobile.manager.server.AppiumServerManager.*;
 
 public class AndroidDriverManager {
-    private static final String APP_PATH = String.valueOf(Path.of(System.getProperty("user.dir"), "src/main/resources", "yummly-8-7.apk"));
+    private static final String APP_PATH = String.valueOf(Path.of(System.getProperty("user.dir"), "src/main/resources/app", "yummly-8-7.apk"));
     private static final ThreadLocal<AndroidDriver> DRIVER = new ThreadLocal<>();
     private static final Logger LOG = LogManager.getLogger(AndroidDriverManager.class);
 
     public static void createAndroidDriver () {
         startServer("android");
-        //        setDriver (new AndroidDriver (getService ().getUrl (), geckoOptionsFirefox ()));
         try {
             setDriver(new AndroidDriver(new URL("http://127.0.0.1:4723/wd/hub"),uiAutomator2OptionsWdio()));
         } catch (final MalformedURLException e) {
@@ -44,20 +41,6 @@ public class AndroidDriverManager {
         }
     }
 
-    private static GeckoOptions geckoOptionsFirefox () {
-        final HashMap<String, Object> firefoxOptions = new HashMap<> ();
-        firefoxOptions.put ("androidPackage", "org.mozilla.firefox");
-        //firefoxOptions.put ("androidDeviceSerial", "EMULATOR32X1X12X0");
-        final GeckoOptions geckoOptions;
-        geckoOptions = new GeckoOptions ().setPlatformName ("mac")
-                .setAutomationName (AutomationName.GECKO)
-                .setMozFirefoxOptions (firefoxOptions)
-                .setAndroidStorage ("app")
-                .setAcceptInsecureCerts (true)
-                .setNoReset (false);
-        return geckoOptions;
-    }
-
     private static void setDriver (final AndroidDriver driver) {
         AndroidDriverManager.DRIVER.set(driver);
     }
@@ -68,61 +51,19 @@ public class AndroidDriverManager {
                 .implicitlyWait(Duration.ofSeconds(5));
     }
 
-    //    private static DesiredCapabilities setCapabilities() {
-    //        DesiredCapabilities capabilities = new DesiredCapabilities();
-    //        capabilities.setCapability(MobileCapabilityType.PLATFORM_NAME, Platform.ANDROID);
-    //        capabilities.setCapability(MobileCapabilityType.DEVICE_NAME, "Pixel_5_API_30");
-    //        capabilities.setCapability(MobileCapabilityType.APP, APP_PATH);
-    //        capabilities.setCapability("appPackage", "com.wdiodemoapp");
-    //        capabilities.setCapability("appActivity", "com.wdiodemoapp.MainActivity");
-    //        capabilities.setCapability("noReset", false);
-    //        capabilities.setCapability(MobileCapabilityType.AUTOMATION_NAME, AutomationName.ANDROID_UIAUTOMATOR2);
-    //        return capabilities;
-    //    }
-
-    /*private static UiAutomator2Options uiAutomator2OptionsChrome () {
-
+    private static UiAutomator2Options uiAutomator2OptionsWdio() {
         final UiAutomator2Options uiAutomator2Options;
-        uiAutomator2Options = new UiAutomator2Options().setAvd("Pixel 6 Pro API 35")
-                .setAvdLaunchTimeout(Duration.ofSeconds (300))
-                .setAvdReadyTimeout(Duration.ofSeconds (100))
-                .setDeviceName("pixel_6_pro")
+        uiAutomator2Options = new UiAutomator2Options()
+                .setAvd("Pixel_4_API_35")
+                .setAvdLaunchTimeout(Duration.ofSeconds(300))
+                .setAvdReadyTimeout(Duration.ofSeconds(100))
+                .setDeviceName("emulator-5554")
                 .setAutomationName(AutomationName.ANDROID_UIAUTOMATOR2)
-                .withBrowserName("chrome")
-                .setAutoGrantPermissions(true)
-                .setNoReset(false);
-
-        return uiAutomator2Options;
-    }
-
-    private static UiAutomator2Options uiAutomator2OptionsProverbial () {
-
-        final UiAutomator2Options uiAutomator2Options;
-        uiAutomator2Options = new UiAutomator2Options().setAvd("Pixel_XL_API_33")
-                .setAvdLaunchTimeout (Duration.ofSeconds (300))
-                .setAvdReadyTimeout (Duration.ofSeconds (100))
-                .setDeviceName ("Pixel_XL_API_33")
-                .setAutomationName (AutomationName.ANDROID_UIAUTOMATOR2)
-                .setApp (APP_PATH)
-                .setAutoGrantPermissions (true)
-                .setAppPackage ("com.lambdatest.proverbial")
-                .setAppActivity ("com.lambdatest.proverbial.MainActivity")
-                .setNoReset (false);
-        return uiAutomator2Options;
-    }*/
-
-    private static UiAutomator2Options uiAutomator2OptionsWdio () {
-        final UiAutomator2Options uiAutomator2Options;
-        uiAutomator2Options = new UiAutomator2Options().setAvd("Pixel 6 Pro API 35")
-                .setAvdLaunchTimeout(Duration.ofSeconds (300))
-                .setAvdReadyTimeout(Duration.ofSeconds (100))
-                .setDeviceName("pixel_6_pro")
-                .setAutomationName (AutomationName.ANDROID_UIAUTOMATOR2)
                 .setApp(APP_PATH)
-                .setAppPackage ("com.yummly.android")
-                .setAppActivity ("com.yummly.android.MainActivity")
-                .setNoReset (false);
+                .setNewCommandTimeout(Duration.ofSeconds(300))
+                .setAppPackage("com.yummly.android")
+                .setAppActivity("com.yummly.android.feature.splash.SplashActivity")
+                .setNoReset(false);
         return uiAutomator2Options;
     }
-
 }
