ca956c821bb3a0cd662eaa0c5b86d0ec958de44d
Ziv Gadri <ziv.automation@gmail.com>
2024-07-17 21:41:32 +0300

    Adding content to MainPage and MainPageHelper

--- src/main/java/web/manager/pages/MainPage.java
+++ src/main/java/web/manager/pages/MainPage.java
@@ -2,20 +2,25 @@ package web.manager.pages;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import org.openqa.selenium.Dimension;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 import web.manager.PageObject;
-import web.manager.pageHelpers.MainPageHelper;
 
 public class MainPage extends PageObject {
+
     private static final Logger logger = LogManager.getLogger(MainPage.class);
+
     @FindBy(xpath = "//button[contains(@class,'acceptDefaultCookieFirstVisit')]")
     private WebElement acceptAllCookiesBtn;
 
-    public MainPage(WebDriver driver, String xmUrl) {
-        super(driver);
+    @FindBy(className = "fa fa-bars")
+    private WebElement mainMenuHamburgerIcon;
+
+    public MainPage(WebDriver driver, String xmUrl, Dimension screenSize) {
+        super(driver, screenSize);
         navigateToUrl(xmUrl);
         PageFactory.initElements(driver, this);
     }
@@ -23,5 +28,8 @@ public class MainPage extends PageObject {
     public WebElement getAcceptAllCookiesBtn() {
         return acceptAllCookiesBtn;
     }
+
+    public WebElement getMainMenuHamburgerIcon() { return mainMenuHamburgerIcon; }
+
 
 }
