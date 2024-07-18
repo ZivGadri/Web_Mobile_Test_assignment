56a309c2b2c5d7394d080ebc63e860b85bd17d56
Ziv Gadri <ziv.automation@gmail.com>
2024-07-18 04:06:30 +0300

    WebTestsWeb update

--- src/test/java/web/WebTestsWeb.java
+++ src/test/java/web/WebTestsWeb.java
@@ -2,11 +2,11 @@ package web;
 
 import org.testng.annotations.Test;
 
-public class WebTests extends TestFlows {
+public class WebTestsWeb extends WebTestFlow {
 
-    public WebTests() {}
+    public WebTestsWeb() {}
 
-    public WebTests(int width, int height) {
+    public WebTestsWeb(int width, int height) {
         this.width = width;
         this.height = height;
     }
