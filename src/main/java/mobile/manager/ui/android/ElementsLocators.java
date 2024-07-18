6e21657ce36d7b37d2e18edf02042fef5574868b
Ziv Gadri <ziv.automation@gmail.com>
2024-07-18 04:00:14 +0300

    Adding ElementsLocators

---
+++ src/main/java/mobile/manager/ui/android/ElementsLocators.java
@@ -0,0 +1,16 @@
+package mobile.manager.ui.android;
+
+import org.openqa.selenium.By;
+
+public class ElementsLocators {
+    public static final By askMeLaterBtn = new By.ById("com.yummly.android:id/skip_view"); // Initial popup
+    public static final By noThanksBtn = new By.ByXPath("//android.widget.TextView[@text=\"No Thanks\"]"); // "No Thanks" button
+    public static final By inviteFriendsNoThanksBtn = new By.ById("com.yummly.android:id/no_thanks_button"); // Invite friends "No Thanks" button
+    public static final By searchField = new By.ByXPath("//android.widget.TextView[@resource-id=\"search_field\"]"); // Search field
+    public static final By searchFieldInput = new By.ById("com.yummly.android:id/search_src_text"); // Search input field
+    public static final By firstSearchResult = new By.ById("com.yummly.android:id/clickFeedbackView"); // First search result
+    public static final By ingredientsLabel = new By.ByXPath("//android.widget.TextView[@resource-id=\"com.yummly.android:id/text1\" and @text=\"Ingredients\"]"); // Ingredients label
+    public static final By ingredientsList = new By.ByClassName("androidx.compose.ui.viewinterop.ViewFactoryHolder"); // Ingredients list
+    public static final By lastIngredientPlusBtn = new By.ById("com.yummly.android:id/related_recipe_favorite"); // Last ingredient plus button - to be found using parent element
+    public static final By buyBtn = new By.ByXPath("//android.widget.TextView[@text=\"Buy\"]"); // Buy button
+}
